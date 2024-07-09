1fa89c9fbf861c5f1ee0de3f14c4734d013040a3
Emrah <emrahketzo@gmail.com>
2024-07-09 20:01:03 +0200

    analytics 2

--- portofolio/src/App.jsx
+++ portofolio/src/App.jsx
@@ -18,8 +18,8 @@ const App = () => {
                 <Route path='/' element={<Home />} />
                 {/* <Route path='/about' element={<h1>About</h1>} />
                 <Route path='/contact' element={<h1>Contact</h1>} /> */}
-                <Analytics />
             </Routes>
+            <Analytics />
         </Router>
     </main>
   )
